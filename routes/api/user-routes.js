fa9b03050bff7b76ce7f34bb9f65f973cce6f621
Marcia Skousen <marciaskousen@Marcias-iMac.local>
2021-04-20 21:23:27 -0700

    add hashed password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -54,8 +54,9 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   
-    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    // pass in req.body instead to only update what's passed through
     User.update(req.body, {
+      individualHooks: true,
       where: {
         id: req.params.id
       }
